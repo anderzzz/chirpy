fa7fddd734667f97d583ec8270984c27b5cf1a56
anderzzz <ohrn.anders@gmail.com>
2021-04-28 11:19:56 +0200

    Basically functional trainer method in place

--- learner_1dconv.py
+++ learner_1dconv.py
@@ -5,6 +5,7 @@ import sys
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
+from torch import optim
 
 from model_1dconv import AudioModel1DAbdoli
 from dataset import ChirpyDataset
@@ -19,11 +20,16 @@ class AudioLearner1DConv(object):
     def __init__(self,
                  db_rootdir, subfolder,
                  loader_batch_size=16, num_workers=0,
-                 f_out=sys.stdout):
+                 f_out=sys.stdout, save_tmp_name='model_in_training',
+                 optimizer='SGD', lr=0.001, momentum=0.9, weight_decay=0.0, betas=(0.9,0.999),
+                 scheduler_step_size=10, scheduler_gamma=0.1):
 
         self.inp_f_out = f_out
+        self.inp_save_tmp_name = save_tmp_name
         self.inp_loader_batch_size = loader_batch_size
         self.inp_num_workers = num_workers
+
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
         transform = Compose([AudioDownSampleTransform(16000),
                              AudioRandomChunkTransform(run_time=5000, append_method='cycle'),
@@ -37,23 +43,52 @@ class AudioLearner1DConv(object):
         self.model = AudioModel1DAbdoli(n_classes=10)
         self.criterion = MajorityVoter(ensemble_size=9)
 
-        self.optimizer = None
-        self.lr_scheduler = None
+        if optimizer == 'SGD':
+            self.optimizer = optim.SGD(self.model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
+        elif optimizer == 'Adam':
+            self.optimizer = optim.Adam(self.model.parameters(), lr=lr, weight_decay=weight_decay, betas=betas)
+        elif optimizer == 'AdamW':
+            self.optimizer = optim.AdamW(self.model.parameters(), lr=lr, weight_decay=weight_decay, betas=betas)
+        else:
+            raise ValueError('Unknown optimizer type: {}'.format(optimizer))
+
+        self.lr_scheduler = optim.lr_scheduler.StepLR(self.optimizer,
+                                                      step_size=scheduler_step_size,
+                                                      gamma=scheduler_gamma)
+
+    def save_model(self, model_path):
+        '''Save encoder state dictionary
+        Args:
+            model_path (str): Path and name to file to save state dictionary to. The filename on disk is this argument
+                appended with suffix `.tar`
+        '''
+        torch.save({'model_state': self.model.state_dict()}, '{}.tar'.format(model_path))
 
     def train(self, n_epochs):
         '''Bla bla
 
         '''
-        self.model.train()
         for epoch in range(n_epochs):
             print ('Epoch {}/{}...'.format(epoch + 1, n_epochs), file=self.inp_f_out)
 
+            self.model.train()
+            running_loss = 0.0
             for inputs in self.dataloader:
-                print (inputs['audio'].shape)
-                mini_classes = self.model(inputs['audio'].float())
-                print (mini_classes.shape)
+                size_batch = inputs['audio'].size(0)
+                audio = inputs['audio'].float().to(self.device)
+
+                self.optimizer.zero_grad()
+                mini_classes = self.model(audio)
+
                 loss = self.criterion(mini_classes, inputs['label'])
-                raise RuntimeError
+                loss.backward()
+                self.optimizer.step()
+                self.lr_scheduler.step()
+
+                running_loss += loss.item() * size_batch
+
+            running_loss = running_loss / float(len(self.dataset))
+            self.save_model(self.inp_save_tmp_name)
 
 
 def test1():
